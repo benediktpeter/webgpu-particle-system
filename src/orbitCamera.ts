fa4ad3665937a99875ed3ca5b39b1b80436f750a
Benedikt Peter <e0171283@student.tuwien.ac.at>
2022-08-15 17:43:47 +0200

    implemented camera rotation

--- src/orbitCamera.ts
+++ src/orbitCamera.ts
@@ -1,38 +1,83 @@
-import {glMatrix, mat4, vec3} from "gl-matrix";
-
-var createOrbitCamera = require("orbit-camera")
-
+import {glMatrix, mat4, quat, vec3} from "gl-matrix";
 
 export class OrbitCamera {
 
-    _orbitCamera : any;
-    _projectionMatrix : mat4;
-    _viewMatrix: mat4;
+    private _position : vec3;
+    private _target: vec3;
+    private _up: vec3
 
-    ZOOM_FACTOR = 0.001;
+    private _rotationQuat = quat.create();
+
+    _projectionMatrix : mat4;
+    _viewMatrix: mat4 = mat4.create();
+
+    readonly ZOOM_FACTOR = 0.001;
+    readonly MAX_ZOOM = 500;
+    readonly MIN_ZOOM = 0.01;
+    readonly MOVE_FACTOR = 1.0;
+
+    private _prevMouseX: number = 0;
+    private _prevMouseY: number = 0;
+
 
     constructor(eye: vec3, center: vec3, up: vec3, fov: number = 60, aspect: number = 16.0/9, near: number = 0.001, far: number = 1000.0) {
-        this._orbitCamera = createOrbitCamera(eye, center, up);
+        this._position = eye;
+        this._target = center;
+        this._up = up;
+
         this._projectionMatrix = OrbitCamera.perspectiveWebGpuFormat(glMatrix.toRadian(fov), aspect, near, far)
-        this._viewMatrix = this._orbitCamera.view();
+        this.updateViewMatrix();
 
         const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;
         canvas.onwheel = (evt) => {
             this.onMouseWheel(evt.deltaY)
         };
+        canvas.addEventListener('mousemove', this.onMouseMove);
     }
 
     public getViewProjectionMatrix() : mat4 {
-        this._orbitCamera.view(this._viewMatrix)
+        this.updateViewMatrix()
         let viewProjectionMatrix = mat4.create();
         mat4.multiply(viewProjectionMatrix, this._projectionMatrix, this._viewMatrix);
         return viewProjectionMatrix;
     }
 
     private onMouseWheel(deltaY : number) : void {
-        this._orbitCamera.zoom(-deltaY * this.ZOOM_FACTOR);
-        console.log("new matrix: " + this._orbitCamera.view())
+        this._position[2] += deltaY * this.ZOOM_FACTOR;
+        this._position[2] = Math.min(this.MAX_ZOOM, this._position[2]);
+        this._position[2] = Math.max(this.MIN_ZOOM, this._position[2]);
     }
+
+    private updateViewMatrix() {
+        const translationMatrix = mat4.create();
+        const rotationMatrix = mat4.create();
+
+        mat4.lookAt(translationMatrix, this._position, this._target, this._up);
+        mat4.fromQuat(rotationMatrix, this._rotationQuat);
+
+        this._viewMatrix = translationMatrix;
+        this._viewMatrix = mat4.multiply(this._viewMatrix, translationMatrix, rotationMatrix);
+    }
+
+    private rotate(angleX: number, angelY: number, angleZ: number): void {
+        let offsetQuat = quat.create();
+        offsetQuat = quat.fromEuler(offsetQuat, angleX, angelY, angleZ);
+        quat.multiply(this._rotationQuat, offsetQuat, this._rotationQuat);
+        this.updateViewMatrix();
+    }
+
+    private onMouseMove = (event: MouseEvent): void => {
+        let currentX = event.clientX;
+        let currentY = event.clientY;
+        if (event.buttons === 1) {
+            let offsetX = (currentX - this._prevMouseX) * this.MOVE_FACTOR;
+            let offsetY = (currentY - this._prevMouseY) * this.MOVE_FACTOR;
+            this.rotate(0.0, offsetX, 0.0);
+            this.rotate(offsetY, 0.0, 0.0);
+        }
+        this._prevMouseX = currentX;
+        this._prevMouseY = currentY;
+    };
 
     // creates the perspective matrix in the format used by WebGPU (z in range [0,1] rather than [-1,1])
     private static perspectiveWebGpuFormat(fov: number, aspect: number, near: number, far: number) {
@@ -65,4 +110,30 @@ export class OrbitCamera {
     }
 
 
+    get position(): vec3 {
+        return this._position;
+    }
+
+    set position(value: vec3) {
+        this._position = value;
+        this.updateViewMatrix()
+    }
+
+    get target(): vec3 {
+        return this._target;
+    }
+
+    set target(value: vec3) {
+        this._target = value;
+        this.updateViewMatrix()
+    }
+
+    get up(): vec3 {
+        return this._up;
+    }
+
+    set up(value: vec3) {
+        this._up = value;
+        this.updateViewMatrix()
+    }
 }
